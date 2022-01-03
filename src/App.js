ea6b450fd55ea7a7dac184592751b3db7554e907
dazedcodes <techwithmaya@gmail.com>
2022-01-02 18:08:07 -0600

    adding joke generator project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import VeryFunnyDad from './VeryFunnyDad';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <VeryFunnyDad/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
